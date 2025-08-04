cd86e091f3259058efdf67d266e38f87c8ffb4f7
Madhankumar-10 <madhanmano.in@gmail.com>
2025-08-04 12:19:58 +0530

    C Programming Commit

---
+++ HACKER RANK/WEEK 1/Plus Minus.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    float a=0,b=0,c=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>=1){
+            a++;
+        }
+        else if (arr[i]==0) {
+            c++;
+        }
+        else {
+            b++;
+        }
+    }
+    float x = a/n;
+    float y = b/n;
+    float z = c/n;
+    printf("%.6f\n%.6f\n%.6f",x,y,z);
+}
